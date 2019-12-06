521929ce22bfe9373e80aef218388ae797389900
gordonpm <gordonm0815@gmail.com>
2019-12-05 23:45:54 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public")); // to serve public folder which contains css files
+app.set("view engine", "ejs"); // all templates are ejs
+
+var bikes = ["Emonda", "Madone", "Dogma", "TCR"];
+
+app.get("/", function(req,res) {
+    res.render("home");
+}); 
+
+
+
+app.post("/addbike", function(req,res) {
+    var newBike = req.body.newbike;
+    bikes.push(newBike);
+    res.redirect("/bikes");
+});
+
+app.get("/bikes", function(req,res) {
+    res.render("bikes", {bikes: bikes});
+});
+
+// app.listen(process.env.PORT, function() {
+//     console.log("Server started " + process.env.PORT + process.env.IP);
+// });
+
+app.listen(3000, () => {
+	console.log("Server started on port 3000");
+});
