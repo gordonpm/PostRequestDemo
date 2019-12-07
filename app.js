8d79fe3f20e1e9958d4c9809608b9ba1040b16e1
gordonpm <gordonm0815@gmail.com>
2019-12-06 18:58:08 -0800

    replaced var declartations with let and const

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static("public")); // to serve public folder which contains css files
 app.set("view engine", "ejs"); // all templates are ejs
 
-var bikes = ["Emonda", "Madone", "Dogma", "TCR"];
+let bikes = ["Emonda", "Madone", "Dogma", "TCR"];
 
 app.get("/", function(req,res) {
     res.render("home");
 }); 
 
-
-
 app.post("/addbike", function(req,res) {
-    var newBike = req.body.newbike;
+    let newBike = req.body.newbike;
     bikes.push(newBike);
     res.redirect("/bikes");
 });
